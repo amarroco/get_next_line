96e934c23f74ea1b98f6bbaa10de173e1589f4ec
Alexis MARROCOS <amarroco@e3r9p20.clusters.42paris.fr>
2023-01-13 21:09:53 +0100

    ok on va voir

--- get_next_line.c
+++ get_next_line.c
@@ -6,56 +6,31 @@
 /*   By: amarroco <amarroco@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 13:27:22 by amarroco          #+#    #+#             */
-/*   Updated: 2022/12/21 06:07:20 by amarroco         ###   ########.fr       */
+/*   Updated: 2023/01/13 20:55:25 by amarroco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void	ft_read(int fd, char **m)
-{
-	char		*b;
-	int			i;
-	
-	b = ft_calloc(BUFFER_SIZE + 1);
-	if (!b)
-		return ;
-	i = 1;
-	while (((*m && ft_strchr(*m, '\n') == -1) || !*m) && i > 0)
-	{
-		i = read(fd, b, BUFFER_SIZE);
-		printf("b : %s\n", b);
-		if (i < 0)
-			return (free(b), free(m));
-		b[i] = 0;
-		*m = ft_strjoin_free(*m, b);
-		if (!m)
-			return (free(b));
-	}
-	printf("read : %s\n", *m);
-	free(b);
-}
-
-char	*ft_substr_free(char *s, int start, int len)
+char	*ft_substr_free(char *m, int start, int len)
 {
 	char	*d;
 
-	d = ft_substr(s, start, len);
+	d = ft_substr(m, start, len);
 	if (!d)
 		return (NULL);
-	free(s);
+	free (m);
 	return (d);
 }
 
-char	*ft_strjoin_free(char *s1, char *s2)
+char	*ft_strjoin_free(char *m, char *b)
 {
 	char	*d;
-
-	d = ft_strjoin(s1, s2);
+	
+	d = ft_strjoin(m, b);
 	if (!d)
 		return (NULL);
-	if (s1)
-		free(s1);
+	free (m);
 	return (d);
 }
 
@@ -63,25 +38,30 @@ char	*get_next_line(int fd)
 {
 	static char	*m;
 	char		*d;
-	
+	char		*b;
+	int			i;
+
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	ft_read(fd, &m);
-	if (!m)
+	b = malloc(BUFFER_SIZE + 1);
+	if (!b)
 		return (NULL);
-	printf("read ok\n");
-	if (ft_strchr(m, '\n') == -1)
+	i = BUFFER_SIZE;
+	while (ft_strchr(*m, '\n') == -1 && i > 0)
 	{
-		printf("return derniere ligne\n");
-		return (m);
+		ft_bzero(b);
+		i = read(fd, b, BUFFER_SIZE);
+		if (i < 0)
+			return (free(b), free(m));
+		m = ft_strjoin_free(m, b);
+		if (!m)
+			return (free(b));
 	}
+	if (ft_strchr(m, '\n') == -1)
+		return (m);
 	d = ft_substr(m, 0, ft_strchr(m, '\n'));
 	if (!d)
-		return (NULL);
-	printf("d ok\n");
+		return (free(m));
 	m = ft_substr_free(m, ft_strchr(m, '\n') + 1, ft_strlen(m));
-	if (!m)
-		return (NULL);
-	printf("m ok\n");
 	return (d);
 }
